cac42179425ec7cd56b63d4d4a054c17d76b5954
kiblik <tomas@kubla.sk>
2024-04-06 02:38:32 +0200

    Ruff: add pyupgrade (#9755)

--- unittests/test_rest_framework.py
+++ unittests/test_rest_framework.py
@@ -111,7 +111,7 @@ def format_url(path):
     return f"{BASE_API_URL}{path}"
 
 
-class SchemaChecker():
+class SchemaChecker:
     def __init__(self, components):
         self._prefix = []
         self._has_failed = False
@@ -272,7 +272,7 @@ class TestType(Enum):
     CONFIGURATION_PERMISSIONS = 3
 
 
-class BaseClass():
+class BaseClass:
     class RESTEndpointTest(DojoAPITestCase):
         def __init__(self, *args, **kwargs):
             DojoAPITestCase.__init__(self, *args, **kwargs)
@@ -1055,7 +1055,7 @@ class FilesTest(DojoAPITestCase):
                 # Save the ID of the newly created file object
                 self.url_levels[level] = response.data.get('id')
         #  Test the download
-        with open(f'{str(self.path)}/scans/acunetix/one_finding.xml', 'r') as file:
+        with open(f'{str(self.path)}/scans/acunetix/one_finding.xml') as file:
             file_data = file.read()
         for level, file_id in self.url_levels.items():
             response = self.client.get(f'/api/v2/{level}/files/download/{file_id}/')
